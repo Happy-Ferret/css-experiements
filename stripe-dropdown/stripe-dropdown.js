54c55071a0baa58bef56ba841571ab7332fba975
Amie Chen <achen@signal.co>
2017-04-29 18:43:49 -0500

    [hodepodge:stripe-dropdown] refactor js to be mostly using css

--- stripe-dropdown/stripe-dropdown.js
+++ stripe-dropdown/stripe-dropdown.js
@@ -3,20 +3,16 @@ $(function () {
   var $mainNav = $('.nav-main li');
   var $dropdownList = $('.dropdown-list');
 
-  $mainNav
-    .on('mouseover', function() {
-      if ($(this).hasClass('library-trigger')) {
-        $dropdownContent.css({'transform': 'translateX(700px)', 'opacity': '1'});
-        $dropdownList.css({'transform': 'translateX(0px)'});
-      } else if ($(this).hasClass('findme-trigger')) {
-        $dropdownContent.css({'transform': 'translateX(800px)', 'opacity': '1'});
-        $dropdownList.css({'transform': 'translateX(-180px)'});
-      } else {
-        $dropdownContent.css({'transform': 'translateX(600px)', 'opacity': '1'});
-        $dropdownList.css({'transform': 'translateX(180px)'});
-      }
-    })
-    .on('mouseout', function() {
-      $dropdownContent.css({'opacity': '0'});
+
+  $mainNav.on('mouseover', function() {
+      var type = $(this).data('trigger');
+      $mainNav.removeClass('active');
+      $(this).addClass('active')
+      $dropdownContent.removeClass('active projects library findme').addClass('active ' + type);
+    });
+
+  $dropdownContent.on('mouseleave', function() {
+      $(this).removeClass('active');
+      $mainNav.removeClass('active')
     });
 });
